dcbda8fcc01b458be45f834578a5a39730c6f5ed
Hexiro <42787085+Hexiro@users.noreply.github.com>
2022-02-02 19:26:21 -0500

    refactor: separate progress bars, multithreading

--- __main__.py
+++ __main__.py
@@ -5,9 +5,12 @@ import json
 import logging
 import os
 import pathlib
+import queue
+import threading
 import time
 import uuid
 from collections import defaultdict
+from json import JSONDecodeError
 from typing import TYPE_CHECKING, Final
 
 import dotenv
@@ -16,6 +19,7 @@ import rich
 import spotipy
 import youtubesearchpython
 from requests_toolbelt import MultipartEncoder
+from rich import traceback
 from rich.logging import RichHandler
 from rich.progress import Progress
 
@@ -24,13 +28,15 @@ from typehints.general import Track, Playlist
 if TYPE_CHECKING:
     from typehints.general import TrackDict
     from typehints.youtube import YoutubeResult
-    from typehints.musi import MusiPlaylist, MusiVideo, MusiBackupResponse, MusiItem
+    from typehints.musi import MusiPlaylist, MusiVideo, MusiItem, MusiBackupResponse
     from typehints.spotify import SpotifyTrack, SpotifyPlaylistItem, SpotifyPlaylist, SpotifyLikedSong
 
 # TODO: bulk search youtube videos
 
 dotenv.load_dotenv(".env")
 console = rich.get_console()
+
+traceback.install()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -69,7 +75,9 @@ spotify.me()
 
 # fetched from spotify
 spotify_playlists: list[SpotifyPlaylist] = []
+spotify_playlist_tracks: dict[str, list[SpotifyTrack]] = {}
 spotify_liked_songs: list[SpotifyLikedSong] = []
+spotify_liked_songs_tracks: list[SpotifyTrack] = []
 
 # from cache, and global song catalog
 tracks: list[Track] = []
@@ -84,29 +92,30 @@ musi_playlists: list[MusiPlaylist] = []
 musi_library_items: list[MusiItem] = []
 
 if data_cache_path.is_file():
-    with open(data_cache_path) as file:
-        cached_data: list[TrackDict] = json.load(file)
-    logger.debug(f"{cached_data=}")
-    for track in cached_data:
-        deserialized = Track(**track, is_from_cache=True)
-        tracks.append(deserialized)
-    logger.debug(f"{tracks=}")
-
-if not SPOTIFY_FIRST_TIME_SETUP:
-    logger.info("Done!")
+    try:
+        with open(data_cache_path) as file:
+            cached_data: list[TrackDict] = json.load(file)
+        logger.debug(f"{cached_data=}")
+        for track in cached_data:
+            deserialized = Track(**track, is_from_cache=True)
+            tracks.append(deserialized)
+        logger.debug(f"{tracks=}")
+    except JSONDecodeError:
+        pass
 
 
 def cache():
+    logger.debug("caching!")
     cache_items: list[TrackDict] = [c.to_dict() for c in tracks]
 
-    with open("cache/data.cache.json", "w") as file:
+    with open(data_cache_path, "w") as file:
         json.dump(cache_items, file, indent=4)
 
 
 atexit.register(cache)
 
 
-def search(spotify_track: SpotifyTrack) -> Track:
+def search(spotify_track: SpotifyTrack, cache_only: bool = False) -> Track:
     """
     search cache and if not found search youtube.
     """
@@ -115,7 +124,7 @@ def search(spotify_track: SpotifyTrack) -> Track:
 
     cached_track = search_cache(artist, song)
 
-    if cached_track:
+    if cached_track or cache_only:
         return cached_track
     else:
         return search_youtube(artist, song)
@@ -159,38 +168,22 @@ def search_youtube(artist: str, song: str) -> Track:
     return track
 
 
-console.clear()
+# querying spotify...
 with Progress(console=console, transient=True) as progress:
-    task_query_spotify_liked_songs = progress.add_task("[green]Querying Spotify For Liked Songs...", total=1)
+    task_query_spotify = progress.add_task("[green]Querying Spotify...", total=2)
+
     spotify_liked_songs = spotify.current_user_saved_tracks()["items"]
-    progress.advance(task_query_spotify_liked_songs)
-
-    task_searching_youtube_liked_songs = progress.add_task(
-        "[cyan]Searching Youtube for Liked Songs...", advance=1, total=len(spotify_liked_songs)
-    )
-
-    # task_sending_to_musi = progress.add_task("[green]Sending Data to Musi...", start=False)
 
     for liked_song in spotify_liked_songs:
         spotify_track = liked_song["track"]
-        track = search(spotify_track)
-        library.append(track)
-        progress.advance(task_searching_youtube_liked_songs)
+        spotify_liked_songs_tracks.append(spotify_track)
 
-    task_query_spotify_playlists = progress.add_task("[green]Querying Spotify For Playlists...", total=1)
+    progress.advance(task_query_spotify)
     spotify_playlists = spotify.current_user_playlists()["items"]
-    progress.advance(task_query_spotify_playlists)
-
-    spotify_playlist_tracks: dict[str, list[SpotifyTrack]] = {}
 
     for spotify_playlist in spotify_playlists:
-        playlist_name = spotify_playlist["name"]
         playlist_id = spotify_playlist["id"]
-
-        logger.info(f"Starting to search playlist, {playlist_name}")
-        logger.info("Fetching tracks... ")
-
-        # fetch tracks
+        playlist_name = spotify_playlist["name"]
 
         results = spotify.playlist_items(playlist_id)
         items: list[SpotifyPlaylistItem] = results["items"]
@@ -198,16 +191,57 @@ with Progress(console=console, transient=True) as progress:
             results = spotify.next(results)
             items.extend(results["items"])
 
-        spotify_tracks: list[SpotifyTrack] = [item["track"] for item in items]
-        spotify_playlist_tracks[playlist_name] = spotify_tracks
-        logger.info("Done!")
+        spotify_playlist_tracks[playlist_name] = [item["track"] for item in items]
 
-    values = list(spotify_playlist_tracks.values())
-    lengths = [len(v) for v in values]
+    progress.advance(task_query_spotify)
 
-    task_searching_youtube_playlists = progress.add_task(
-        "[cyan]Searching Youtube for Playlist Songs...", advance=1, total=sum(lengths)
-    )
+# searching youtube...
+with Progress(console=console, transient=True) as progress:
+    task_searching_youtube = progress.add_task("[red]Searching YouTube...", start=False, advance=1)
+    spotify_tracks_to_search = queue.Queue(maxsize=0)
+
+    for spotify_track in spotify_liked_songs_tracks:
+        spotify_tracks_to_search.put(spotify_track)
+
+    for spotify_tracks in spotify_playlist_tracks.values():
+        for spotify_track in spotify_tracks:
+            spotify_tracks_to_search.put(spotify_track)
+
+    progress.update(task_searching_youtube, total=spotify_tracks_to_search.qsize())
+    progress.start_task(task_searching_youtube)
+
+
+    def search_and_advance():
+        thread = threading.current_thread()
+        while spotify_tracks_to_search.qsize() > 0:
+            if not thread.is_alive():
+                return
+            spotify_track = spotify_tracks_to_search.get()
+            search(spotify_track)
+            progress.advance(task_searching_youtube)
+            spotify_tracks_to_search.task_done()
+
+
+    # python threading w/o blocking KeyboardInterrupt
+    # reference: http://gregoryzynda.com/python/developer/threading/2018/12/21/interrupting-python-threads.html
+
+    threads: list[threading.Thread] = []
+    for i in range(5):
+        try:
+            thread = threading.Thread(target=search_and_advance, daemon=True)
+            threads.append(thread)
+            thread.start()
+        except KeyboardInterrupt as e:
+            thread.join()
+            raise e
+
+    for thread in threads:
+        while thread.is_alive():
+            thread.join(0.5)
+
+# sending to musi...
+with Progress(console=console, transient=True) as progress:
+    task_sending_to_musi = progress.add_task("[orange]Sending to Musi...", advance=1, total=4)
 
     for spotify_playlist in spotify_playlists:
         playlist_name = spotify_playlist["name"]
@@ -220,51 +254,59 @@ with Progress(console=console, transient=True) as progress:
         spotify_tracks = spotify_playlist_tracks[playlist_name]
 
         for spotify_track in spotify_tracks:
-            track = search(spotify_track)
+            track = search(spotify_track, cache_only=True)
             playlists[playlist].append(track)
-            progress.advance(task_searching_youtube_playlists)
 
-for playlist, playlist_tracks in playlists.items():
-    # add tracks to global video catalog
-    for track in playlist_tracks:
+    progress.advance(task_sending_to_musi)
+
+    for playlist, playlist_tracks in playlists.items():
+        # add tracks to global video catalog
+        for track in playlist_tracks:
+            musi_items.append(track.to_musi_video())
+        # add playlist
+        musi_playlist_items = [track.to_musi_item(position) for position, track in enumerate(playlist_tracks)]
+        musi_playlist: MusiPlaylist = {
+            "ot": "custom",
+            "name": playlist.name,
+            "type": "user",
+            "date": int(time.time()),
+            "items": musi_playlist_items,
+            "ciu": playlist.cover_url,
+        }
+        musi_playlists.append(musi_playlist)
+
+    progress.advance(task_sending_to_musi)
+
+    for position, track in enumerate(library):
+        # add track to global video catalog
         musi_items.append(track.to_musi_video())
-    # add playlist
-    musi_playlist_items = [track.to_musi_item(index) for index, track in enumerate(playlist_tracks)]
-    musi_playlist: MusiPlaylist = {
-        "ot": "custom",
-        "name": playlist.name,
-        "type": "user",
-        "date": int(time.time()),
-        "items": musi_playlist_items,
-        "ciu": playlist.cover_url,
+        # add library-specific videos
+        musi_library_items.append(track.to_musi_item(position))
+
+    progress.advance(task_sending_to_musi)
+
+    logger.debug(f"{musi_library_items=}")
+    logger.debug(f"{musi_items=}")
+    logger.debug(f"{musi_playlists=}")
+
+    payload = {
+        "library": {"ot": "custom", "items": musi_library_items, "name": "My Library", "date": time.time()},
+        "playlist_items": musi_items,
+        "playlists": musi_playlists,
     }
-    musi_playlists.append(musi_playlist)
+    multipart_encoder = MultipartEncoder(
+        fields={"data": json.dumps(payload), "uuid": str(uuid.uuid4())},
+        boundary=f"Boundary+Musi{uuid.uuid4()}",
+    )
+    headers = {
+        "Content-Type": multipart_encoder.content_type,
+        "User-Agent": "Musi/25691 CFNetwork/1206 Darwin/20.1.0",
+    }
+    resp = requests.post("https://feelthemusi.com/api/v4/backups/create", data=multipart_encoder, headers=headers)
+    backup: MusiBackupResponse = resp.json()
 
-for position, track in enumerate(library):
-    # add track to global video catalog
-    musi_items.append(track.to_musi_video())
-    # add library-specific videos
-    musi_library_items.append(track.to_musi_item(position))
+    progress.advance(task_sending_to_musi)
 
-logger.debug(f"{musi_library_items=}")
-logger.debug(f"{musi_items=}")
-logger.debug(f"{musi_playlists=}")
+    logger.info(f"{backup['success']} code={backup['code']}")
 
-payload = {
-    "library": {"ot": "custom", "items": musi_library_items, "name": "My Library", "date": time.time()},
-    "playlist_items": musi_items,
-    "playlists": musi_playlists,
-}
-multipart_encoder = MultipartEncoder(
-    fields={"data": json.dumps(payload), "uuid": str(uuid.uuid4())},
-    boundary=f"Boundary+Musi{uuid.uuid4()}",
-)
-headers = {
-    "Content-Type": multipart_encoder.content_type,
-    "User-Agent": "Musi/25691 CFNetwork/1206 Darwin/20.1.0",
-}
-resp = requests.post("https://feelthemusi.com/api/v4/backups/create", data=multipart_encoder, headers=headers)
-backup: MusiBackupResponse = resp.json()
-
-logger.info(f"{backup['success']} code={backup['code']}")
 console.print(f"[red]Success! use code, [bold]{backup['code']}[/bold] on Musi to restore your songs from Spotify.")
