0b16d0416090a71c4e39a6a52254af37b2a7b9a7
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-04-10 23:30:33 -0400

    refactor: better `spotify-to-musi` logging

--- spotify_to_musi/main.py
+++ spotify_to_musi/main.py
@@ -9,24 +9,27 @@ import tracks_cache
 
 
 async def main() -> None:
+    spotify_to_musi_rich_text = f"[bold][green4]SPOTIFY-[/green4][grey53]TO[/grey53]-[dark_orange3]MUSI[/dark_orange3][/bold]"
+
     spotify_playlists = await spotify.fetch_spotify_playlists()
-    rich.print(f"[bold blue]SPOT-TO-MUSI[/bold blue] loaded {len(spotify_playlists)} spotify playlists")
+    rich.print(f"{spotify_to_musi_rich_text} loaded {len(spotify_playlists)} spotify playlists")
     spotify_liked_tracks = await spotify.fetch_spotify_liked_tracks()
-    rich.print(f"[bold blue]SPOT-TO-MUSI[/bold blue] loaded {len(spotify_liked_tracks)} spotify liked tracks")
+    rich.print(f"{spotify_to_musi_rich_text} loaded {len(spotify_liked_tracks)} spotify liked tracks")
 
     # TODO: de-deduplicate tracks when requesting ytm
+    # TODO: fix bug where query is ' - '
 
     playlists = await spotify.covert_spotify_playlists_to_playlists(spotify_playlists)
-    rich.print(f"[bold blue]SPOT-TO-MUSI[/bold blue] fetched {len(spotify_playlists)} playlists")
+    rich.print(f"{spotify_to_musi_rich_text} fetched {len(spotify_playlists)} playlists")
     liked_tracks = spotify.covert_spotify_tracks_to_tracks(spotify_liked_tracks)
-    rich.print(f"[bold blue]SPOT-TO-MUSI[/bold blue] fetched {len(liked_tracks)} liked tracks")
+    rich.print(f"{spotify_to_musi_rich_text} fetched {len(liked_tracks)} liked tracks")
 
     await tracks_cache.load_cached_youtube_tracks()
 
     youtube_playlists = await youtube.convert_playlists_to_youtube_playlists(playlists)
-    rich.print(f"[bold blue]SPOT-TO-MUSI[/bold blue] fetched {len(spotify_playlists)} youtube playlists")
+    rich.print(f"{spotify_to_musi_rich_text} fetched {len(spotify_playlists)} youtube playlists")
     youtube_liked_tracks = await youtube.convert_tracks_to_youtube_tracks(liked_tracks)
-    rich.print(f"[bold blue]SPOT-TO-MUSI[/bold blue] fetched {len(youtube_liked_tracks)} youtube liked tracks")
+    rich.print(f"{spotify_to_musi_rich_text} fetched {len(youtube_liked_tracks)} youtube liked tracks")
 
     all_youtube_tracks = list(youtube_liked_tracks)
     for youtube_playlist in youtube_playlists:
