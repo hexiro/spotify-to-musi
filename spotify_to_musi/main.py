bc6be0ef0a63fa6ed819f72b399212c76073738c
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-04-13 19:21:13 -0400

    feat: start progress bar w/ Spotify queries

--- spotify_to_musi/main.py
+++ spotify_to_musi/main.py
@@ -7,41 +7,43 @@ import musi
 import youtube
 import tracks_cache
 
+from rich.progress import Progress
+
 
 async def main() -> None:
     spotify_to_musi_rich_text = (
         f"[bold][green4]SPOTIFY-[/green4][grey53]TO[/grey53]-[dark_orange3]MUSI[/dark_orange3][/bold]"
     )
 
-    spotify_playlists = await spotify.fetch_spotify_playlists()
-    rich.print(f"{spotify_to_musi_rich_text} loaded {len(spotify_playlists)} spotify playlists")
-    spotify_liked_tracks = await spotify.fetch_spotify_liked_tracks()
-    rich.print(f"{spotify_to_musi_rich_text} loaded {len(spotify_liked_tracks)} spotify liked tracks")
+    with Progress() as progress:
+        spotify_playlists, spotify_liked_tracks = await spotify.query_spotify(progress)
 
-    # TODO: de-deduplicate tracks when requesting ytm
+        return
 
-    playlists = spotify.covert_spotify_playlists_to_playlists(spotify_playlists)
-    rich.print(f"{spotify_to_musi_rich_text} fetched {len(spotify_playlists)} playlists")
-    liked_tracks = spotify.covert_spotify_tracks_to_tracks(spotify_liked_tracks)
-    rich.print(f"{spotify_to_musi_rich_text} fetched {len(liked_tracks)} liked tracks")
+        # TODO: de-deduplicate tracks when requesting ytm
 
-    await tracks_cache.load_cached_youtube_tracks()
+        playlists = spotify.covert_spotify_playlists_to_playlists(spotify_playlists)
+        rich.print(f"{spotify_to_musi_rich_text} fetched {len(spotify_playlists)} playlists")
+        liked_tracks = spotify.covert_spotify_tracks_to_tracks(spotify_liked_tracks)
+        rich.print(f"{spotify_to_musi_rich_text} fetched {len(liked_tracks)} liked tracks")
 
-    youtube_playlists = await youtube.convert_playlists_to_youtube_playlists(playlists)
-    rich.print(f"{spotify_to_musi_rich_text} fetched {len(spotify_playlists)} youtube playlists")
-    youtube_liked_tracks = await youtube.convert_tracks_to_youtube_tracks(liked_tracks)
-    rich.print(f"{spotify_to_musi_rich_text} fetched {len(youtube_liked_tracks)} youtube liked tracks")
+        await tracks_cache.load_cached_youtube_tracks()
 
-    all_youtube_tracks = list(youtube_liked_tracks)
-    for youtube_playlist in youtube_playlists:
-        all_youtube_tracks.extend(youtube_playlist.tracks)
+        youtube_playlists = await youtube.convert_playlists_to_youtube_playlists(playlists)
+        rich.print(f"{spotify_to_musi_rich_text} fetched {len(spotify_playlists)} youtube playlists")
+        youtube_liked_tracks = await youtube.convert_tracks_to_youtube_tracks(liked_tracks)
+        rich.print(f"{spotify_to_musi_rich_text} fetched {len(youtube_liked_tracks)} youtube liked tracks")
 
-    await tracks_cache.cache_youtube_tracks(all_youtube_tracks)
+        all_youtube_tracks = list(youtube_liked_tracks)
+        for youtube_playlist in youtube_playlists:
+            all_youtube_tracks.extend(youtube_playlist.tracks)
 
-    musi_playlists = musi.convert_playlists_to_musi_playlists(youtube_playlists)
-    musi_library = musi.covert_youtube_tracks_to_musi_library(youtube_liked_tracks)
+        await tracks_cache.cache_youtube_tracks(all_youtube_tracks)
 
-    backup = await musi.upload_to_musi(musi_playlists, musi_library)
+        musi_playlists = musi.convert_playlists_to_musi_playlists(youtube_playlists)
+        musi_library = musi.covert_youtube_tracks_to_musi_library(youtube_liked_tracks)
+
+        backup = await musi.upload_to_musi(musi_playlists, musi_library)
 
     rich.print(f"[bold][dark_orange3]MUSI CODE[/dark_orange3]: [white]{backup.code}[/white][/bold]")
 
--- spotify_to_musi/spotify.py
+++ spotify_to_musi/spotify.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import math
 import os
 import sys
 import aiofiles
@@ -7,7 +8,7 @@ import rich
 
 import pydantic.error_wrappers
 from pydantic import parse_obj_as
-
+from rich.progress import Progress, TaskID
 
 from typings.core import Playlist, Track, Artist
 from typings.spotify import SpotifyTrack, BasicSpotifyPlaylist, SpotifyPlaylist
@@ -49,25 +50,76 @@ async def init() -> None:
     await spotify.populate_user_creds()
 
 
-async def fetch_spotify_playlists() -> list[SpotifyPlaylist]:
+async def query_spotify(progress: Progress) -> tuple[list[SpotifyPlaylist], list[SpotifyTrack]]:
+    await init()
+
+    def description(subtype: str) -> str:
+        return f"[bold][green]Querying Spotify [ [white]{subtype}[/white] ][/green][/bold]... "
+
+    task_id = progress.add_task(description("Playlists"), start=False)
+    spotify_basic_playlists = await fetch_basic_spotify_playlists(task_id=task_id, progress=progress)
+
+    task_id = progress.add_task(description("Liked Songs"), start=False)
+    spotify_liked_tracks = await fetch_spotify_liked_tracks(task_id=task_id, progress=progress)
+
+    task_id = progress.add_task(description("Playlist Tracks"), start=False)
+
+    spotify_playlists = await load_basic_playlists(spotify_basic_playlists, task_id=task_id, progress=progress)
+
+    return spotify_playlists, spotify_liked_tracks
+
+
+async def fetch_basic_spotify_playlists(
+    task_id: TaskID,
+    progress: Progress,
+) -> list[BasicSpotifyPlaylist]:
     await init()
 
     playlists_resp = await spotify.user_playlists()
     playlists_items = playlists_resp["items"]  # type: ignore
 
+    limit = 50
+
+    total_tracks: int = playlists_resp["total"]  # type: ignore
+    total = math.ceil(total_tracks / limit)
+
+    progress.update(task_id, total=total, completed=0)
+    progress.start_task(task_id)
+
+    async def load_user_playlists(offset: int, limit: int):
+        try:
+            return await spotify.user_playlists(offset=offset, limit=limit)
+        finally:
+            progress.update(task_id, advance=1)
+
     # load all pages
     offset = 0
     while playlists_resp["next"]:  # type: ignore
         offset = len(playlists_items)
-        playlists_resp = await spotify.user_playlists(offset=offset)
+        playlists_resp = await load_user_playlists(offset=offset, limit=limit)
         playlists_items.extend(playlists_resp["items"])  # type: ignore
 
     spotify_basic_playlists = [BasicSpotifyPlaylist(**p) for p in playlists_items]
+    return spotify_basic_playlists
 
+
+async def load_basic_playlists(
+    basic_spotify_playlists: t.Iterable[BasicSpotifyPlaylist],
+    *,
+    task_id: TaskID,
+    progress: Progress,
+) -> list[SpotifyPlaylist]:
     playlist_tasks: list[asyncio.Task[SpotifyPlaylist]] = []
 
-    for basic_playlist in spotify_basic_playlists:
-        coro = basic_playlist_to_playlist(basic_playlist)
+    total = 0
+    for basic_playlist in basic_spotify_playlists:
+        total += math.ceil(basic_playlist.tracks.total / 50)
+
+    progress.update(task_id, total=total, completed=0)
+    progress.start_task(task_id)
+
+    for basic_playlist in basic_spotify_playlists:
+        coro = basic_playlist_to_playlist(basic_playlist, task_id=task_id, progress=progress)
         task = asyncio.create_task(coro)
         playlist_tasks.append(task)
 
@@ -75,10 +127,15 @@ async def fetch_spotify_playlists() -> list[SpotifyPlaylist]:
     return spotify_playlists
 
 
-async def basic_playlist_to_playlist(basic_playlist: BasicSpotifyPlaylist) -> SpotifyPlaylist:
+async def basic_playlist_to_playlist(
+    basic_playlist: BasicSpotifyPlaylist,
+    *,
+    task_id: TaskID,
+    progress: Progress,
+) -> SpotifyPlaylist:
     await init()
 
-    spotify_tracks = await load_basic_playlist_tracks(basic_playlist)
+    spotify_tracks = await load_basic_playlist_tracks(basic_playlist, task_id=task_id, progress=progress)
 
     playlist = SpotifyPlaylist(
         name=basic_playlist.name,
@@ -92,16 +149,30 @@ async def basic_playlist_to_playlist(basic_playlist: BasicSpotifyPlaylist) -> Sp
         tracks=spotify_tracks,
     )
 
+    rich.print(f"[bold green]SPOTIFY:[/bold green] Loaded Playlist ({playlist.name}) [{len(spotify_tracks)} tracks]")
+
     return playlist
 
 
-async def load_basic_playlist_tracks(basic_spotify_playlist: BasicSpotifyPlaylist) -> list[SpotifyTrack]:
+async def load_basic_playlist_tracks(
+    basic_spotify_playlist: BasicSpotifyPlaylist,
+    *,
+    task_id: TaskID,
+    progress: Progress,
+) -> list[SpotifyTrack]:
     await init()
 
     spotify_tracks_items_tasks: list[asyncio.Task[list[dict]]] = []
 
-    for offset in range(0, basic_spotify_playlist.tracks.total, 20):
-        coro = spotify.playlist_tracks(playlist_id=basic_spotify_playlist.id, offset=offset)
+    async def load_playlist_tracks(offset: int, limit: int):
+        try:
+            return await spotify.playlist_tracks(playlist_id=basic_spotify_playlist.id, offset=offset, limit=limit)
+        finally:
+            progress.update(task_id, advance=1)
+
+    limit = 50
+    for offset in range(0, basic_spotify_playlist.tracks.total, limit):
+        coro = load_playlist_tracks(offset=offset, limit=limit)
         task = asyncio.create_task(coro)
 
         spotify_tracks_items_tasks.append(task)  # type: ignore
@@ -142,19 +213,40 @@ def spotify_track_items_to_spotify_tracks(spotify_track_items: list[dict[str, t.
     return spotify_tracks
 
 
-async def fetch_spotify_liked_tracks() -> list[SpotifyTrack]:
+async def fetch_spotify_liked_tracks(
+    task_id: TaskID,
+    progress: Progress,
+) -> list[SpotifyTrack]:
     await init()
 
     liked_tracks_resp = await spotify.user_tracks()
     liked_tracks_items = liked_tracks_resp["items"]  # type: ignore
 
+    limit = 50
+
+    total_tracks: int = liked_tracks_resp["total"]  # type: ignore
+    total = math.ceil(total_tracks / limit)
+
+    progress.update(task_id, total=total, completed=0)
+    progress.start_task(task_id)
+
+    async def load_user_tracks(offset: int, limit: int):
+        try:
+            return await spotify.user_tracks(offset=offset, limit=limit)
+        finally:
+            progress.update(task_id, advance=1)
+
     offset = 0
     while liked_tracks_resp["next"]:  # type: ignore
         offset = len(liked_tracks_items)
-        liked_tracks_resp = await spotify.user_tracks(offset=offset)
+        print(f"{offset=} {total=}")
+        liked_tracks_resp = await load_user_tracks(offset=offset, limit=limit)
         liked_tracks_items.extend(liked_tracks_resp["items"])  # type: ignore
 
     liked_tracks: list[SpotifyTrack] = spotify_track_items_to_spotify_tracks(liked_tracks_items)
+
+    rich.print(f"[bold green]SPOTIFY:[/bold green] Loaded Liked Songs ({len(liked_tracks)} tracks)")
+
     return liked_tracks
 
 
@@ -182,7 +274,7 @@ def covert_spotify_track_to_track(spotify_track: SpotifyTrack) -> Track:
     return track
 
 
-def covert_spotify_playlists_to_playlists(spotify_playlists: list[SpotifyPlaylist]) -> tuple[Playlist, ...]:
+def covert_spotify_playlists_to_playlists(spotify_playlists: t.Iterable[SpotifyPlaylist]) -> tuple[Playlist, ...]:
     return tuple(covert_spotify_playlist_to_playlist(p) for p in spotify_playlists)
 
 
@@ -193,8 +285,8 @@ def covert_spotify_tracks_to_tracks(spotify_tracks: t.Iterable[SpotifyTrack]) ->
 if __name__ == "__main__":
 
     async def main():
-        spotify_playlists = await fetch_spotify_playlists()
-        spotify_liked_tracks = await fetch_spotify_liked_tracks()
+        with Progress() as progress:
+            spotify_playlists, spotify_liked_tracks = await query_spotify(progress)
 
         playlists = covert_spotify_playlists_to_playlists(spotify_playlists)
         liked_tracks = covert_spotify_tracks_to_tracks(spotify_liked_tracks)
