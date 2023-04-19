0b1ceb99247239a17fc3f4b72501efdc1d1daaa0
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-04-18 20:19:02 -0400

    refactor: install rich tracebacks & ignore warnings
    ignoring warnings is mainly done for `RequestsDependencyWarning` as requests isn't actively used as a dep but is rather installed as a sub-dep and imported

--- spotify_to_musi/__init__.py
+++ spotify_to_musi/__init__.py
@@ -4,7 +4,20 @@ import pathlib
 import sys
 
 import rich.traceback
+import warnings
+
+from requests.exceptions
 
 rich.traceback.install()
-
 sys.path.append(str(pathlib.Path(__file__).parent))
+
+
+# requests is installed as a sub-dependency of pyfy (& maybe more packages that are sync and async)
+# pyfy imports requests even when it's not being used which will cause a  `RequestsDependencyWarning` to be raised on older versions of python
+# there should be no issue with ignoring this warning as it's only raised through requests which is not actually being used
+
+# i wasn't able to import `RequestsDependencyWarning` from `requests.exceptions` as it would raise the error immediately
+# before the warning filter could be applied
+# but it hopefully should be okay ignoring all warnings :3
+
+warnings.filterwarnings("ignore")
